a76a2e7d26007b92537f9103dae2a2522c9603bc
giuspugl <giuspugl@sissa.it>
2016-05-11 15:37:49 +0200

    added apodization routines to the clouds

--- cloudmodel/mapping.py
+++ cloudmodel/mapping.py
@@ -2,7 +2,57 @@ import healpy as hp
 import h5py as h5
 import numpy as np 
 
-def do_healpy_map(Pop,nside,fname):
+
+def gaussian_apodization(x,d,p=3):
+	"""
+	smooth the emissivity of the cloud with a gaussian , centered in the center of the cloud
+	and with sigma defined in such a way that: 
+
+	.. math:: 
+	
+		\epsilon(d) = 10 ^{-p} \epsilon_0
+
+	where ``d`` is the border of the cloud, i.e. the radius of the cloud, ``p`` is the parameter to set 
+	the width of the cloud, sigma as expected is related to ``p``. the higher is p
+	"""
+	sigma	=	d/np.sqrt(2.*p*np.log(10))
+	y		= 	(x/(np.sqrt(2)*sigma))**2
+	
+	return np.exp(-y)
+
+def cosine_apodization(x,d): 
+	"""
+	smooth the emissivity of the cloud with a cosine, with the following relation: 
+	.. math:: 
+	
+		\epsilon(x) = \epsilon_0 \cos(\frac{\pi}{2} \frac{x}{d})
+
+	where ``d`` is the border of the cloud, i.e. the radius of the cloud, in such a 
+	way that the  :math:`\epsilon(d)=0`. 
+	"""
+	return np.cos(x/d*np.pi/2.)
+
+def distance_from_cloud_center(theta,phi,theta_c,phi_c): 
+	"""
+	given a position of one pixel :math:`(\theta,\phi)` within the cloud compute the arclength 
+	of the pixel from the center, onto a unitary sphere.  by considering scalar products of vectors 
+	to the points  on the sphere to get the angle :math:`\psi` between them. .
+	see for reference : 
+	`Arclength on a sphere <http://math.stackexchange.com/questions/231221/great-arc-distance-between-two-points-on-a-unit-sphere>`_
+	"""
+	cos1	=	np.cos(theta_c)
+	sin1	=	np.sin(theta_c)
+	cos2	=	np.cos(theta)
+	sin2	= 	np.sin(theta)
+	cosphi	=	np.cos(phi_c - phi)
+
+	psi 	=	np.arccos( cos1 *cos2 + sin1*sin2 *cosphi )
+
+	return psi
+
+
+
+def do_healpy_map(Pop,nside,fname,apodization='cos'):
 	N=Pop.n
 	mapcloud=np.zeros(hp.nside2npix(nside))
 	sizekpc=Pop.L/1.e3
@@ -10,9 +60,21 @@ def do_healpy_map(Pop,nside,fname):
 	for i in xrange(N):
 		vec 		=	Pop.healpix_vecs[i]
 		angularsize =	sizekpc[i]/Pop.d_sun[i]
-
+		em_c=Pop.W[i]
+		theta_c,phi_c= hp.vec2ang(vec)
 		listpix=hp.query_disc(nside,vec,angularsize)
-		mapcloud[listpix]	+= Pop.W[i]
+
+		theta_pix,phi_pix=hp.pix2ang(nside, listpix )
+
+		distances= distance_from_cloud_center(theta_pix,phi_pix,theta_c,phi_c)
+
+		if apodization == 'cos':
+			profile = cosine_apodization(distances,angularsize)
+		if apodization == 'gaussian':
+			profile = gaussian_apodization(distances,angularsize)
+
+		mapcloud[listpix]	+= em_c*profile
+
 	hp.write_map(fname,mapcloud)
 	return mapcloud
 
